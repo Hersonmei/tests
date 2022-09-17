7a350313bcdea246bd8ecb2f621e08e2772539d3
Hersonmei <herson.me@hotmail.com>
2022-09-17 17:33:29 -0300

    Atualização endereço audio

--- js/sounds.js
+++ js/sounds.js
@@ -1,8 +1,8 @@
 export function Sounds(){
-    const audioForest = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Floresta.wav?raw=true")
-    const audioRain = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Chuva.wav?raw=true")
-    const audioCafeteria = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Cafeteria.wav?raw=true")
-    const audioFireplace = new Audio("https://github.com/Hersonmei/Explorer/blob/main/Nivel%205/Desafio%2001/audio/Lareira.wav?raw=true")
+    const audioForest = new Audio("../audio/Floresta.wav?raw=true")
+    const audioRain = new Audio("../audio/Chuva.wav?raw=true")
+    const audioCafeteria = new Audio("../audio/Cafeteria.wav?raw=true")
+    const audioFireplace = new Audio("../audio/Lareira.wav?raw=true")
 
     
     function cancelAudio(){
